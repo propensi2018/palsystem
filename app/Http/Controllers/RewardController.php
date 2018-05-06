99655857125ca794677f7c008bfdb3fe64dc8ad4
farhannp9 <farhan.np9@gmail.com>
2018-05-07 00:36:57 +0700

    statistics add color

---
+++ app/Http/Controllers/RewardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use App\MessageReceived;
+use App\Message;
+use App\Salesperson;
+use App\Branch;
+use App\Region;
+use App\Manager;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Input;
+use App\Http\Controllers\File;
+use DateTime;
+
+
+class RewardController extends Controller
+{
+  public function compareSalesperson(){
+    if(){
+
+    }
+  }
+
+    return view('dummyReward',compact('nameSp'));
+  }
+  public function compareProduct(){
+
+  }
+}
