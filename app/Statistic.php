982a62d10b1b13cb2902144929128d662a2b1438
farhannp9 <farhan.np9@gmail.com>
2018-05-06 16:30:05 +0700

    fix appt

--- app/Statistic.php
+++ app/Statistic.php
@@ -6,25 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Statistic extends Model
 {
-
+  /*
+  return the amount of product sold in the month and date
+  */
   protected function calculateProduct($month, $year, $product_type_id) {
     $amounts = DB::table('transactions')
     ->join('product_lists', 'transactions.id_pl', '=', 'product_lists.id')
     ->join('product_list_assocs', 'product_lists.id', '=', 'product_list_assocs.product_list_id')
-    ->where('product_list_assocs.id_ptype', '=', '')
+    ->where('product_list_assocs.id_ptype', '=', $product_type_id)
     ->where('created_at', '>=', $year . "-" . $month . "-1 00:00:00")
     ->where('created_at', '<', $year . "-" . ($month + 1) . "-1 00:00:00")
     ->select('amount');
     return $amounts -> sum();
   }
 
+  /*
+  return the set of labels needed by the chart.js
+  */
+  public function returnLabels() {
+    date_default_timezone_set("Asia/Bangkok");
+    $m = date('m');
+    $array = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    $returner = []
+    for ($i = 0; i < $m; $i++) {
+      array_push($returner, $array[$i]);
+    }
+    return $returner;
+
+  }
+
+  /*
+  return an array of production of the month
+  */
   public function calculateYear($year, $product_type_id) {
     date_default_timezone_set("Asia/Bangkok");
     $m = date('m');
     // return $today1;
     $returner = array();
 
-    for ($i = 0 , $i < $m; $i++) {/
+    for ($i = 0 , $i < $m; $i++) {
       array_push($returner, calculateProduct($i, $year, $product_type_id));
     }
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,50 +16,39 @@
                   </div>
                 </div>
                 <div class="row reminder-body">
-                  <canvas id="myChart" width="400" height="100"></canvas>
+                  <canvas id="myChart" width="400" height="500"></canvas>
 <script>
 var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      title : {
-        display : true,
-        text : 'Product'
+var myChart = new Chart(document.getElementById("myChart"), {
+  type: 'line',
+  data: {
+    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{
+        data: [86,114,106,106,107,111,133,221,783,2478],
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: 'origin'
+      }, {
+        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        label: "Asia",
+        borderColor: "#8e5ea2",
+        fill: 1
+      }, {
+        data: [168,170,178,190,203,276,408,547,675,734],
+        label: "Europe",
+        borderColor: "#3cba9f",
+        fill: 2,
+        Boundary : 'start'
       },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'World population per region (in millions)'
     }
-});
-</script>
+  }
+});</script>
                 </div>
               </div>
             </div>
