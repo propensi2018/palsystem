6fefe282ab4ef9e4e248da51ca554d593bba1a3d
farhannp9 <farhan.np9@gmail.com>
2018-05-16 11:16:15 +0700

    hyeah now we got business

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -65,6 +65,7 @@ class ReminderController extends Controller
                 }
             }
         }
+        return 'something i dont know';
 
         date_default_timezone_set("Asia/Bangkok");
         $today1 = date('Y-m-d');
@@ -75,7 +76,7 @@ class ReminderController extends Controller
         $statistics = new Statistic;
         $data = $statistics->product_data();
         $labels = $statistics->returnLabels();
-
+        // return [$data];
         //handling statistik salesperson
         $dataSales = $statistics-> sales_data($id);
 
